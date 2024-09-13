fa02e1ffcc79801953e32b4541d9a0dd99825175
DmitriyPyanzin <lysy_41@mail.ru>
2024-09-13 08:34:59 +0300

    AddTime

---
+++ MyFirstWebApplications/Models/WeatherForecast.cs
@@ -0,0 +1,47 @@
+﻿namespace MyFirstWebApplications.Models
+{
+    /// <summary>
+    /// Прогноз погоды Forecast
+    /// </summary>
+    public class WeatherForecast
+    {
+        /// <summary>
+        /// Дата измерения
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Время изменения
+        /// </summary>
+        public TimeOnly time { get; set; }
+
+        /// <summary>
+        /// Температура в градусах Цельсия
+        /// </summary>
+        public int TemperatureC { get; set; }
+
+        /// <summary>
+        /// Температура в градусах Форенгейта
+        /// </summary>
+        public int TemperatureF
+        {
+            get { return 32 + (int)(TemperatureC / 0.5556); }
+        }
+
+        /// <summary>
+        /// Температура в градусах Реомюра
+        /// </summary>
+        public int TemperatureR
+        {
+            get { return (int)(TemperatureC * 0.8); }
+        }
+
+        /// <summary>
+        /// Температура в градусах Кельвина
+        /// </summary>
+        public int TemperatureK
+        {
+            get { return (int)(TemperatureC + 273.15); }
+        }
+    }
+}
